fab4be47143b79feef7d094f6503d4d04f82f28e
caiyancheng <19307140030@fudan.edu.cn>
2024-12-31 20:30:20 +0000

    2024-12-31

--- Camera_Calibration/undistortion_raw.py
+++ Camera_Calibration/undistortion_raw.py
@@ -1,3 +1,4 @@
+import HDRutils
 import os
 import numpy as np
 import json
@@ -14,15 +15,15 @@ image_read_path = r'E:\sony_pictures\ArUco_Petterns_JPEG_3/'
 image_save_path = r'E:\sony_pictures\ArUco_Petterns_JPEG_3_undistortion/'
 os.makedirs(image_save_path, exist_ok=True)
 
-img_name = 'DSC00004.JPG'
-img = cv.imread(os.path.join(image_read_path, img_name))
+img_name = 'DSC00001.arw'
+img = HDRutils.imread(os.path.join(image_read_path, img_name)) #[H,W,3] #[4920,7362,3], 16位, 0 - 65535
 h,  w = img.shape[:2]
 newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
 # undistort
 dst = cv.undistort(img, mtx, dist, None, newcameramtx)
-
-# crop the image
+# X = 1
+# # crop the image
 x, y, w, h = roi
 dst = dst[y:y+h, x:x+w]
-cv.imwrite(os.path.join(image_save_path, img_name + 'undistortion.png'), dst)
+HDRutils.imwrite(os.path.join(image_save_path, img_name + 'undistortion.exr'), dst)
